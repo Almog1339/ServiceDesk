1a3bd7c051aa524a224e46a171f7662b1c72b8c3
Almog AmAos <33369377+Almog1339@users.noreply.github.com>
2018-09-15 18:15:53 +0300

    changes
    Added login page

---
+++ Servicedesk/wwwroot/DBHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Servicedesk.wwwroot
+{
+    public class DBHelper
+    {
+        public static string CONN_STRING;
+        public static string ConnectionString
+        {
+            set
+            {
+                if (CONN_STRING == null)
+                    CONN_STRING = value;
+            }
+        }
+    }
+    public static class Extension
+    {
+        public static string GetStingOrNull(this SqlDataReader dr,int i)
+        {
+            if (dr.isDBNull(i))
+                return null;
+            return dr.GetString(i);
+        }
+    }
+}
