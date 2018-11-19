536b301078a579136ca07a55e8843ce83739c6e6
Almog AmAos <33369377+Almog1339@users.noreply.github.com>
2018-11-19 16:24:18 +0200

    f
    nv

--- Servicedesk/wwwroot/Js/Login.js
+++ Servicedesk/wwwroot/Js/Login.js
@@ -1,24 +1,21 @@
 ﻿function btnSend() {
-    function btnSend() {
-        var password = document.getElementById("Password").value;
-        var user = document.getElementById("User").value;
+    var password = document.getElementById("Password").value;
+    var user = document.getElementById("User").value;
 
-        var users = {
-            UserName: user,
-            Password: password
-        };
-        console.log(JSON.stringify(users));
+    var users = {
+        UserName: user,
+        Password: password
+    };
 
-        var url = "https://localhost:44317/api/login";
-        var req = {
-            type: "POST",
-            url: url,
-            contentType: "application/json",
-            dataType: "json",
-            data: JSON.stringify(users)
-        };
-        $.ajax(req).then(function (data) {
-            console.log(data);
-        });
-    }
+    var url = "api/login";
+    var req = {
+        type: "POST",
+        url: url,
+        contentType: "application/json",
+        dataType: "json",
+        data: JSON.stringify(users)
+    };
+    $.ajax(req).then(function (data) {
+        console.log(data);
+    });
 }
